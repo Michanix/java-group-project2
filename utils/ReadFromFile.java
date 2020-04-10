76c9aa8daf76ceb921ac0b2273b1757b708d8ce5
mihhail.m <mihhail.matisinets@gmail.com>
2020-04-10 15:55:40 +0300

    class for handling reads from  from file

---
+++ utils/ReadFromFile.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFromFile {
+  public List<String> readFileToList(String path) throws Exception {
+    // Takes in text file with some data and returns it as ArrayList<>()
+    // Assumed that data on seperate line
+    BufferedReader in = null;
+    List<String> listOFText = new ArrayList<>();
+    try {
+      in = new BufferedReader(new FileReader(path));
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (in != null) {
+        in.close();
+      }
+    }
+    return listOFText;
+  }
+}
