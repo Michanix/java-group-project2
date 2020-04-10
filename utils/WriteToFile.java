efe5eb5750de4f28801b70a490f512c899102ef7
mihhail.m <mihhail.matisinets@gmail.com>
2020-04-10 21:30:04 +0300

    formatting

--- utils/WriteToFile.java
+++ utils/WriteToFile.java
@@ -7,13 +7,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
 public class WriteToFile {
   public void writePlayerToFile(Player player) {
     try (PrintWriter output = new PrintWriter(new FileWriter("players/players.txt", true))) {
-        output.println(player.toString());
+      output.println(player.toString());
 
-    } catch (FileNotFoundException e){
+    } catch (FileNotFoundException e) {
       System.err.println("File not found." + e);
     } catch (IOException e) {
       e.printStackTrace();
