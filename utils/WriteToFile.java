a1568486baf016ac1bb4613978353536ecb6380f
mihhail.m <mihhail.matisinets@gmail.com>
2020-04-10 21:28:10 +0300

    something something here done

--- utils/WriteToFile.java
+++ utils/WriteToFile.java
@@ -1,4 +1,22 @@
 package utils;
 
+import entities.Player;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
 public class WriteToFile {
+  public void writePlayerToFile(Player player) {
+    try (PrintWriter output = new PrintWriter(new FileWriter("players/players.txt", true))) {
+        output.println(player.toString());
+
+    } catch (FileNotFoundException e){
+      System.err.println("File not found." + e);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
