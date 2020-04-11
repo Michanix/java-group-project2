2bc5be9521c8aefeb424552b34c1da7223fcb535
mihhail.m <mihhail.matisinets@gmail.com>
2020-04-11 19:06:01 +0300

    goddess class added

---
+++ entities/monster/Goddess.java
@@ -0,0 +1,24 @@
+package entities.monster;
+
+public class Goddess extends Monster {
+
+  public Goddess(int hp, double phyDmg, double magDmg,
+                 double phyDef, double magDef, int expPer) {
+    super(hp, phyDmg, magDmg, phyDef, magDef, expPer);
+  }
+
+  @Override
+  public void attack() {
+    System.out.println("Goddess basic attack");
+  }
+
+  @Override
+  public void specialAttack() {
+    System.out.println("Goddess special attack");
+  }
+
+  @Override
+  public String toString() {
+    return super.toString();
+  }
+}
