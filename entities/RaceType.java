5272195018510ab1f2f0c318a5e95211459349cf
mihhail.m <mihhail.matisinets@gmail.com>
2020-04-11 12:28:59 +0300

    special spells for races

--- entities/RaceType.java
+++ entities/RaceType.java
@@ -1,18 +1,50 @@
 package entities;
 
 public enum RaceType {
-  ELF("Elf"),
-  DWARF("Dwarf"),
-  HUMAN("Human"),
-  HIGHELF("High Elf"),
-  MOUNTAINDWARF("Mountain Dwarf"),
-  ORC("Orc");
+  ELF("Elf") {
+    @Override
+    public void specialCast() {
+      System.out.println("Elf special magic spell!");
+    }
+  },
+  DWARF("Dwarf") {
+    @Override
+    public void specialCast() {
+      System.out.println("Dwarf special spell!");
+    }
+  },
+  HUMAN("Human") {
+    @Override
+    public void specialCast() {
+      System.out.println("Human special spell!");
+    }
+  },
+  HIGHELF("High Elf") {
+    @Override
+    public void specialCast() {
+      System.out.println("Human special spell!");
+    }
+  },
+  MOUNTAINDWARF("Mountain Dwarf") {
+    @Override
+    public void specialCast() {
+      System.out.println("Mountain Dwarf special spell!");
+    }
+  },
+  ORC("Orc") {
+    @Override
+    public void specialCast() {
+      System.out.println("Orc special spell!");
+    }
+  };
 
   private final String raceType;
 
   RaceType(String raceType) {
     this.raceType = raceType;
   }
+
+  public abstract void specialCast();
 
   @Override
   public String toString() {
