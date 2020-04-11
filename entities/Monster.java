6997bc7214100e106d4dc6551a85d54bff9caff9
mihhail.m <mihhail.matisinets@gmail.com>
2020-04-11 14:48:24 +0300

    monsters added!

---
+++ entities/Monster.java
@@ -0,0 +1,43 @@
+package entities;
+
+public abstract class Monster {
+  private final int         hp; // health
+  private final double  phyDmg; // Physical damage
+  private final double  magDmg; // Magical damage
+  private final double  phyDef; // Defense against physical damage
+  private final double  magDef; // Same but magical
+  private final int     expPer; // experience per killed monster
+
+  public Monster(int hp, double phyDmg, double magDmg,
+                 double phyDef, double magDef, int expPer) {
+    this.hp =         hp;
+    this.phyDmg = phyDmg;
+    this.magDmg = magDmg;
+    this.phyDef = phyDef;
+    this.magDef = magDef;
+    this.expPer = expPer;
+  }
+
+  // Ordinary hit
+  public abstract void attack();
+  // Special attack
+  public abstract void specialAttack();
+
+  @Override
+  public String toString() {
+    return "Monster{"
+        + "hp="
+        + hp
+        + ", phyDmg="
+        + phyDmg
+        + ", magDmg="
+        + magDmg
+        + ", phyDef="
+        + phyDef
+        + ", magDef="
+        + magDef
+        + ", expPer="
+        + expPer
+        + '}';
+  }
+}
---
+++ entities/Troll.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Troll extends Monster {
+
+  public Troll(
+          int hp, double phyDmg, double magDmg,
+          double phyDef, double magDef, int expPer) {
+    super(hp, phyDmg, magDmg, phyDef, magDef, expPer);
+  }
+
+  @Override
+  public void attack() { System.out.println("Troll attacks!");}
+
+  @Override
+  public void specialAttack() { System.out.println("Troll special attack!");}
+}
