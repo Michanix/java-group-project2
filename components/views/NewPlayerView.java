9ff48b04030c01c204c7d530721cc05223b80ed0
mihhail.m <mihhail.matisinets@gmail.com>
2020-04-15 13:39:15 +0300

    basic functionallity done, now separation of concerns

--- components/views/NewPlayerView.java
+++ components/views/NewPlayerView.java
@@ -1,17 +1,16 @@
 package components.views;
 
-// in progress...
-
 import entities.player.ArmorType;
+import entities.player.Player;
 import entities.player.RaceType;
 import javafx.geometry.Pos;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Tooltip;
-import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextFlow;
+import utils.WriteToFile;
 
 public class NewPlayerView {
   // what player see
@@ -31,20 +30,24 @@ public class NewPlayerView {
   private final Label armorLabel = new Label("Choose armor: ");
   private final Label raceLabel = new Label("Choose race: ");
   private final Label weaponLabel = new Label("Choose weapon:\n Bare Hands");
-  private final Tooltip weapontTT = new Tooltip("New players start without actual weapon");
+  private final Tooltip weaponTT = new Tooltip("New players start without actual weapon");
+  // Buttons
+  private final Button createBtn = new Button("Create and continue");
+  // creating arrays of enum types for later usage
+  private final RaceType[] raceTypes = RaceType.values();
+  private final ArmorType[] armorTypes = ArmorType.values();
+  private final WriteToFile writeToFile = new WriteToFile();
 
-  public NewPlayerView() {
-  }
+  public NewPlayerView() {}
 
   public Pane init() {
-    RaceType[] raceTypes = RaceType.values();
-    ArmorType[] armorTypes = ArmorType.values();
+    int width = 130;
 
     armorChoice.getItems().addAll(ArmorType.values());
     raceChoice.getItems().addAll(RaceType.values());
-    armorChoice.setPrefWidth(130);
-    raceChoice.setPrefWidth(130);
-    nicknameField.setPrefWidth(130);
+    armorChoice.setPrefWidth(width);
+    raceChoice.setPrefWidth(width);
+    nicknameField.setPrefWidth(width);
 
     // Displaying brief description of each race/armor when selected
     raceChoice
@@ -55,29 +58,41 @@ public class NewPlayerView {
               raceDisc.setText(String.valueOf(raceTypes[newValue.intValue()].getDescription()));
             });
     armorChoice
-            .getSelectionModel()
-            .selectedIndexProperty()
-            .addListener(
-                    (observable, oldValue, newValue) -> {
-                      armorDisc.setText(String.valueOf(armorTypes[newValue.intValue()].getDescription()));
-                    });
+        .getSelectionModel()
+        .selectedIndexProperty()
+        .addListener(
+            (observable, oldValue, newValue) -> {
+              armorDisc.setText(String.valueOf(armorTypes[newValue.intValue()].getDescription()));
+            });
 
+    // Creating and saving new Player to a file
+    createBtn.setOnMouseClicked(
+        e -> {
+          writeToFile.writePlayerToFile(
+              Player.createNewPlayer(
+                  nicknameField.getText(), raceChoice.getValue(), armorChoice.getValue()));
+        });
 
-    weaponLabel.setTooltip(weapontTT);
     // setup
     view.setAlignment(Pos.CENTER);
     // name
     view.add(nicknameLabel, 0, 0);
     view.add(nicknameField, 0, 1);
     // race
+    displayRaceText.setTextAlignment(TextAlignment.LEFT);
     view.add(raceLabel, 0, 2);
     view.add(raceChoice, 0, 3);
     view.add(displayRaceText, 1, 3);
     // armor
+    displayArmorText.setTextAlignment(TextAlignment.LEFT);
     view.add(armorLabel, 0, 4);
     view.add(armorChoice, 0, 5);
     view.add(displayArmorText, 1, 5);
+    // weapon
     view.add(weaponLabel, 0, 6);
+    weaponLabel.setTooltip(weaponTT);
+
+    view.add(createBtn, 1, 7);
     view.setVgap(20);
     view.setHgap(20);
     return view;
