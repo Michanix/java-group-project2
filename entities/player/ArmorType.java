d73213ea034c90e9ebb9b751772cda5a8f2c5ffb
mihhail.m <mihhail.matisinets@gmail.com>
2020-04-15 11:05:57 +0300

    description method added

--- entities/player/ArmorType.java
+++ entities/player/ArmorType.java
@@ -1,16 +1,31 @@
 package entities.player;
 
 public enum ArmorType {
-  HEAVYARMOR("Heavy armor"),
-  LIGHTARMOR("Light armor"),
-  GARMENT("Garment");
+  HEAVYARMOR("Heavy armor") {
+    @Override
+    public String getDescription() {
+      return null;
+    }
+  },
+  LIGHTARMOR("Light armor") {
+    @Override
+    public String getDescription() {
+      return null;
+    }
+  },
+  GARMENT("Garment") {
+    @Override
+    public String getDescription() {
+      return null;
+    }
+  };
 
   private final String armorType;
 
   ArmorType(String armorType) {
     this.armorType = armorType;
   }
-
+  public abstract String getDescription();
   @Override
   public String toString() {
     return armorType;
