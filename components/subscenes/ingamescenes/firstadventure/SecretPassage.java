b8ef6a820dee02cfeceae320f9b3ea1ecff16c79
mihhail.m <mihhail.matisinets@gmail.com>
2020-04-25 11:30:34 +0300

    pane for secretPass

---
+++ components/subscenes/ingamescenes/firstadventure/SecretPassage.java
@@ -0,0 +1,24 @@
+package components.subscenes.ingamescenes.firstadventure;
+
+import components.buttons.DefaultButton;
+import entities.player.ArmorType;
+import entities.player.Player;
+import javafx.scene.text.Text;
+import textfiles.TextType;
+import utils.GetRandomType;
+
+public class SecretPassage extends AbstracPane {
+
+  public SecretPassage(Player player, String textname, TextType textType) {
+    super(textname, textType);
+    ArmorType rndArmor = GetRandomType.randomEnum(ArmorType.class);
+    setRight(new Text(String.format("You got %s", rndArmor.toString())));
+    player.setArmorType(rndArmor);
+    DefaultButton backButton = new DefaultButton("Back");
+    backButton.setOnMouseClicked(
+        e -> {
+          getScene().setRoot(new StartPane(player, "begining", TextType.FUNCTIONAL));
+        });
+    setLeft(backButton);
+  }
+}
---
+++ components/subscenes/ingamescenes/firstadventure/TrapEscapePane.java
@@ -0,0 +1,4 @@
+package components.subscenes.ingamescenes.firstadventure;
+
+public class TrapEscapePane {
+}
