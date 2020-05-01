538897b0c550ecb6417204cad50cf1dcc9aab9ac
mihhail.m <mihhail.matisinets@gmail.com>
2020-05-01 14:31:44 +0300

    removed file

--- components/subscenes/ingamescenes/firstadventure/PaneChanger.java
+++
@@ -1,7 +0,0 @@
-package components.subscenes.ingamescenes.firstadventure;
-
-import javafx.scene.control.Button;
-
-public interface PaneChanger {
-    void setPaneOnClick(Button button, AbstracPane pane);
-}
