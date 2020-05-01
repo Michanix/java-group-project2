5bba0fb06816cf373493b6159fdbb80cd4f2c09e
mihhail.m <mihhail.matisinets@gmail.com>
2020-05-01 13:23:36 +0300

    refactoring, extracting functions

--- components/subscenes/ingamescenes/firstadventure/FightingPane.java
+++ components/subscenes/ingamescenes/firstadventure/FightingPane.java
@@ -12,10 +12,10 @@ public class FightingPane extends AbstracPane {
   private final DefaultButton physAttack = new DefaultButton("Physical attack");
   private final DefaultButton magAttack = new DefaultButton("Magical attack");
   private final DefaultButton nextBtn = new DefaultButton("Next");
-  private final FightPaneController fightController = new FightPaneController();
 
   public FightingPane(Player player, Monster monster, String textname) {
     super(player, textname);
+    FightPaneController fightController = new FightPaneController(monster, player);
     TextArea textArea = new TextArea();
     nextBtn.setOnMouseClicked(
         e -> {
@@ -27,8 +27,8 @@ public class FightingPane extends AbstracPane {
     System.out.println(player.getWeaponType());
     System.out.println(player.getArmorType());
 
-    fightController.physAttackController(physAttack, player, monster, textArea);
-    fightController.magAttackController(magAttack, player, monster, textArea);
+    fightController.physAttackController(physAttack, textArea);
+    fightController.magAttackController(magAttack, textArea);
 
     textArea.autosize();
     textArea.setEditable(false);
