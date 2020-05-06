7e9c8c05753b6ba294e3cba24abe89b2fd536834
mihhail.m <mihhail.matisinets@gmail.com>
2020-05-06 11:22:24 +0300

    BufferedReader -> Files.writeString

--- utils/WriteToFile.java
+++ utils/WriteToFile.java
@@ -2,19 +2,24 @@ package utils;
 
 import entities.player.Player;
 
-import java.io.*;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class WriteToFile {
+  private static final String DESTINATION_FILE = "players.txt";
+  private static final Charset ENCODING = StandardCharsets.UTF_8;
+
   public static void writePlayerToFile(Player player) {
-    try (Writer output =
-        new BufferedWriter(
-            new OutputStreamWriter(
-                new FileOutputStream("players.txt", true), StandardCharsets.UTF_8))) {
-      output.append(player.toString()).append("\r\n");
+    try {
+      Files.writeString(
+          Path.of(DESTINATION_FILE), player.toString() + "\n", ENCODING, StandardOpenOption.APPEND);
       System.out.println("Player " + player.getNickname() + " was created.");
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException ex) {
+      ex.printStackTrace();
     }
   }
 }
